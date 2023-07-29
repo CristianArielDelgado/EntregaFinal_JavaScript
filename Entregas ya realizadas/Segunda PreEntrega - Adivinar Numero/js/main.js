0906e95329f8c85d0a20d761e38ea8d2fe9f99e0
Cristian <cristianarieldelgado@gmail.com>
2023-07-29 16:21:19 -0300

    Nuevo proyecto: carrito de compras

---
+++ Entregas ya realizadas/Segunda PreEntrega - Adivinar Numero/js/main.js
@@ -0,0 +1,7 @@
+let nombreUsuario = '';
+
+bienvenidaUsuarios();                 //PONER CUADNO SE TERMINA EL JUEGO
+explicacionJuegoUsuario();
+jugar();
+juegoTerminado(puntuacionJuego, arrayNumElegidos, nAleatorio)
+
--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-let nombreUsuario = '';
-
-bienvenidaUsuarios();                 //PONER CUADNO SE TERMINA EL JUEGO
-explicacionJuegoUsuario();
-jugar();
-juegoTerminado(puntuacionJuego, arrayNumElegidos, nAleatorio)
-
+// bienvenidaUsuario();

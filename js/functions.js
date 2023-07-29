9221dccb923a7413ed6cf49b78ef622a78d5c7ed
Cristian <cristianarieldelgado@gmail.com>
2023-07-29 17:18:18 -0300

    agregamos funcion para saber el id q el usuario decide comprar

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,39 @@
 /* Agregamos los articulos al carrito */
-const btnAgregar = document.querySelectorAll('.btn.btn-success');
-let cantidadComprar = document.querySelector('#cantidadComprar');
-let contador = 0;
-
+const btnAgregar = document.querySelectorAll('.btn.btn-success')
+let cantidadComprar = document.querySelector('#cantidadComprar')
+let contador = 0
 btnAgregar.forEach((btn) => {
   btn.addEventListener('click', () => {
     contador++;
-    actualizarContador();
-   })
- });
+    actualizarContador()
+  })
+});
 const actualizarContador = () => {
-  cantidadComprar.innerText = contador;
+  cantidadComprar.innerText = contador
 };
+
+
+/* Obtener ID del producto */
+const obtenerID = document.querySelectorAll('.btn.btn-success');
+obtenerID.forEach((boton) => {
+    boton.addEventListener('click', () => {
+        const idArticulo = parseInt(boton.id)
+
+        /* Buscamos el precio del articulo */
+        console.log(idArticulo)
+    })
+});
+
+
+
+/* Función para encontrar el precio del artículo */
+// const precioArticulo = (idArt) => {
+//   const articuloEncontrado = articulosJSON.find((articulo) => articulo.id === idArt);
+
+//   if (articuloEncontrado) {
+//       console.log("Precio del artículo:", articuloEncontrado.precio);
+//   } else {
+//       console.log("Artículo no encontrado.");
+//   }
+// };
+

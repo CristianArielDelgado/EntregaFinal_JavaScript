4500bcac444519bc70b017782f01996355e6c783
Cristian <cristianarieldelgado@gmail.com>
2023-07-29 16:56:34 -0300

    codigo para aumentar el carrito de compras

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,3 @@
-// Funcion que le da la bienvenida al usuario, para chequear si se introduce o no.
-// const bienvenidaUsuario = () =>{
-//     let nombreUsuario;
-//     do {
-//         nombreUsuario = prompt('Ingrese su nombre para continuar');
-//     } while (nombreUsuario==null || nombreUsuario.length <=2);
-//     const nombre = document.querySelector('.nombreUsuario');
-//     nombre.innerHTML = `${nombreUsuario}`
-// }
-
-
 /* Agregamos los articulos al carrito */
 const btnAgregar = document.querySelectorAll('.btn.btn-success');
 let cantidadComprar = document.querySelector('#cantidadComprar');
@@ -18,18 +7,8 @@ btnAgregar.forEach((btn) => {
   btn.addEventListener('click', () => {
     contador++;
     actualizarContador();
-  });
-});
+   })
+ });
 const actualizarContador = () => {
   cantidadComprar.innerText = contador;
 };
-
-// const btnDisminuir = document.querySelector('.btn-danger');
-// btnDisminuir.addEventListener('click', () => {
-//   if (contador != 0) {
-//     contador--;
-//   }
-//   actualizarContador();
-// });
-
-
